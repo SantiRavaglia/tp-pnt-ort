9697ecfded2ba39410276bef6977f5a0c3d7426c
SantiRavaglia <santi23062000@gmail.com>
2025-10-28 23:24:28 -0300

    directorios client (front) y server (back), autenticacion api de spotify y endpoint /datos

---
+++ client/src/components/Sidebar.vue
@@ -0,0 +1,13 @@
+<template>
+  <aside class="sidebar">
+    <div class="brand">SpotifyListens</div>
+    <nav class="nav">
+      <a href="/" class="active">Panel</a>
+      <a href="/canciones">Canciones</a>
+      <a href="/artistas">Artistas</a>
+      <a href="/albumes">Álbumes</a>
+      <a href="/playlists">Playlists</a>
+      <a href="/configuracion">Configuración</a>
+    </nav>
+  </aside>
+</template>
